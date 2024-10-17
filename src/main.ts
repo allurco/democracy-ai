df79430e724ee3a796e1fb8f195cae6e78b441d6
Rogers <r@allur.co>
2024-10-17 05:27:49 -0300

    aff: tag manager

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createGtm } from '@gtm-support/vue-gtm'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +11,12 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(
+  createGtm({
+    id: 'GTM-M92N8HMQ',
+    vueRouter: router,
+  }),
+)
 
 app.mount('#app')
